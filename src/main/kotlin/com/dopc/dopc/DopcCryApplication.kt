17ee654a9b8b799f8d9e275ba779618428c689d6
ruyicS2023 <123344125+ruyicS2023@users.noreply.github.com>
2025-01-24 18:02:41 +0900

    Initial commit

---
+++ src/main/kotlin/com/dopc/dopc/DopcCryApplication.kt
@@ -0,0 +1,11 @@
+package com.dopc.dopc
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DopcCryApplication
+
+fun main(args: Array<String>) {
+	runApplication<DopcCryApplication>(*args)
+}
---
+++ src/test/kotlin/com/dopc/dopc/DopcCryApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.dopc.dopc
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DopcCryApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
